c287277591c9d3f31d714dd4d0e6c7a496e9bafe
LUIS EDUARDO COSTA LAURINDO <luis.laurindo@discente.ufma.br>
2021-04-19 01:11:13 -0300

    subscribe at topics and to show coordinates

---
+++ safepet-app/app/src/main/java/br/ufma/lsdi/safepetmobile/CONSTANTES.java
@@ -0,0 +1,13 @@
+package br.ufma.lsdi.safepetmobile;
+
+
+public class CONSTANTES {
+
+    public static String TOPIC_DOG_100 = "dog/100/sensor/gps";
+    public static String TOPIC_HORSE_103 = "horse/103/sensor/gps";
+    public static String TOPIC_DUCK_104 = "duck/104/sensor/gps";
+    public static String TOPIC_CAT_101 = "cat/101/sensor/gps";
+    public static String TOPIC_HORSE_108 = "horse/108/sensor/gps";
+    public static String TOPIC_DUCK_110 = "duck/110/sensor/gps";
+
+}
