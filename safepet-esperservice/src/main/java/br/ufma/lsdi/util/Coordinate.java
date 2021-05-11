d5640e3cd5bb96a1a7f5784eef0b17964abdf699
ANDERSON DE OLIVEIRA COSTA <anderson.oc@ufma.br>
2021-05-10 23:08:51 -0300

    add Coordinate and Parse classes in the utility package

---
+++ safepet-esperservice/src/main/java/br/ufma/lsdi/util/Coordinate.java
@@ -0,0 +1,19 @@
+package br.ufma.lsdi.util;
+
+public class Coordinate {
+    final private Double latitude;
+    final private Double longitude;
+
+    public Coordinate(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+}
